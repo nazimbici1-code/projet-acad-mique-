38772db3f20b5795d99eed81ee9e260c2bfd023f
nazimbici1-code <nazim.bici1@gmail.com>
2026-02-01 11:21:13 +0100

    Implement basic server functionality in Serveur.java

---
+++ Serveur.java
@@ -0,0 +1,34 @@
+@ -0,0 +1,33 @@
+package essaye;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur {
+
+    ServerSocket ss;
+    int port = 3000;
+
+    public Serveur() {
+        System.out.println("Serveur démarré, j'attends des clients...");
+
+        try {
+            ss = new ServerSocket(port);
+
+            while (true) {
+                Socket socket = ss.accept();
+                System.out.println("Nouveau client connecté : " + socket);
+
+                ClientHandler ch = new ClientHandler(socket);
+                ch.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Serveur();
+    }
+}
